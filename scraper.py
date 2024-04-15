bdf2e2178fce44d1a1d471547c39274a2253bf7e
Esme <esmepuzio@ucsb.edu>
2024-04-14 19:38:39 -0700

    URLs secured for Dolan Springs AZ

--- scraper.py
+++ scraper.py
@@ -4,16 +4,29 @@ from bs4 import BeautifulSoup
 '''
 Web scraper to get images from Zillow by location. It worked exactly once.
 To do: resolve error 403 from Zillow.
+In my defense: robots.txt states that the url /homes/fsbo/*_p/$ is allowed!
 '''
 
 def scrape_zillow(locations):
     urls = []
     search = ["fsbo", "for_sale/fore_lt", "for_sale"] #for sale by owner, foreclosure, for sale
+    search = ["for_sale"] #for sale by owner, foreclosure, for sale
     for l in locations:
         for s in search:
-            url = f"https://www.zillow.com/{l}/homes/{s}/*_p/$"
-            headers = { #user agent for epuzio's browser
-                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+            url = f"https://www.zillow.com/{l}/homes/{s}/$"
+            print("URL:", url)
+            headers = { #from https://scrapeops.io/web-scraping-playbook/403-forbidden-error-web-scraping/
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+                "Accept-Language": "en-US,en;q=0.5",
+                "Accept-Encoding": "gzip, deflate",
+                "Connection": "keep-alive",
+                "Upgrade-Insecure-Requests": "1",
+                "Sec-Fetch-Dest": "document",
+                "Sec-Fetch-Mode": "navigate",
+                "Sec-Fetch-Site": "none",
+                "Sec-Fetch-User": "?1",
+                "Cache-Control": "max-age=0",
             }
             response = requests.get(url, headers=headers)
             print("Status:", response.status_code)
@@ -34,3 +47,5 @@ if __name__ == "__main__":
     else:
         locations = sys.argv[1:]
         scrape_zillow(locations)
+
+    #cities: Lake-Havasu-City AZ, dolan springs az
