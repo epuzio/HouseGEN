d9a2274b36602606db564b7064d0ad6750637a4e
E. Puzio <esmepuzio@ucsb.edu>
2024-06-12 17:41:38 -0700

    Update segment.py

--- segment.py
+++ segment.py
@@ -1,7 +1,11 @@
 #https://keras.io/examples/vision/deeplabv3_plus/
 #https://github.com/hiwad-aziz/kitti_deeplab/blob/master/inference.py
+#https://github.com/rishizek/tensorflow-deeplab-v3-plus?tab=readme-ov-file
+#https://github.com/hellochick/ICNet-tensorflow
+#https://www.kaggle.com/code/kerneler/image-segmentation-cityscapes-dataset
 
 import tensorflow as tf
+from zipfile import ZipFile
 from PIL import Image
 from torchvision import transforms
 import matplotlib.pyplot as plt
@@ -9,102 +13,103 @@ from torchdata.datapipes.iter import FileLister, FileOpener
 import matplotlib.pyplot as plt
 import os
 import sys
+import tempfile
+import imageio
 import numpy as np
 import cv2
 
-tf.compat.v1.enable_eager_execution()
-# tf.disable_v2_behavior()
-
-# IMAGE_HEIGHT = 512
-# IMAGE_WIDTH = 512
-IMAGE_HEIGHT = 512
-IMAGE_WIDTH = 1382 #change these
+IMAGE_HEIGHT = 183
+IMAGE_WIDTH = 275
 TF_RECORD_PATH = "output.tfrecord"
 NUM_IMGS = 8
 OUTPUT_PATH = "output_images"
+NUM_CLASSES = 19
 
 def decode_image(image):
     '''
-    Map the image to the [-1, 1] range.
+    Decode the image from the tfrecord file.
     '''
-    image = tf.image.decode_jpeg(image, channels=3)
-    image = tf.image.resize(image, [512, 1382]) #CHANGE THIS.
+    image = tf.io.decode_jpeg(image, channels=3)
     image = tf.reshape(image, [IMAGE_HEIGHT, IMAGE_WIDTH, 3])
-    image = (tf.cast(image, tf.float32) / 127.5) - 1 #Normalize images to [-1, 1]
+    image = tf.cast(image, tf.uint8)
+    image = tf.io.encode_png(image)
     return image
 
 def read_tfrecord(example):
-  '''
-  Decode the image from the tfrecord file using decode_image
-  '''
-  tfrecord_format = {
-    "image": tf.io.FixedLenFeature([], tf.string)
-  }
-  example = tf.io.parse_single_example(example, tfrecord_format)
-  image = decode_image(example['image'])
-  print(image.shape)
-  print("type: ", type(image))
-  return image
-
+    '''
+    Decode the image from the tfrecord file using decode_image
+    '''
+    tfrecord_format = {
+        "image": tf.io.FixedLenFeature([], tf.string)
+    }
+    example = tf.io.parse_single_example(example, tfrecord_format)
+    image = decode_image(example['image'])
+    return image
 
 def load_inference_graph(path):
     with tf.io.gfile.GFile(path, 'rb') as f:
         graph_def = tf.compat.v1.GraphDef()
         graph_def.ParseFromString(f.read())
+        
     with tf.Graph().as_default() as graph:
-        tf.import_graph_def(graph_def, name='prefix')  
+        tf.import_graph_def(graph_def, name="prefix")
+
     return graph
 
 def logits2image(logits):
+    print('logits called')
+    image_classes = 19
     logits = logits.astype(np.uint8)
-    images = [np.zeros([IMAGE_HEIGHT,IMAGE_WIDTH,3],dtype=float)]
+    images = [np.zeros([IMAGE_HEIGHT,IMAGE_WIDTH,3],dtype=float) for _ in range(image_classes)]
+
     for i in range(IMAGE_HEIGHT):
         for j in range(IMAGE_WIDTH):
             img_class = logits[i,j]
             images[img_class][i,j,:] = [255, 255, 255]
     
-    images = images.astype(np.uint8)
+    images = [img.astype(np.uint8) for img in images]
     return images
 
 def slice_imgs():
     print("slicing images")
-    dataset = tf.data.TFRecordDataset(TF_RECORD_PATH)
-    dataset = dataset.map(read_tfrecord) #Unpacks from string to a float32 with correct dims under shape
-    dataset_size = sum(1 for _ in dataset)
-    # dataset = dataset.shuffle(dataset_size).batch(NUM_IMGS)
+    # dataset = tf.data.TFRecordDataset(TF_RECORD_PATH)
+    # dataset = dataset.map(read_tfrecord)
 
-    
     print("loading inference graph")
-    graph = load_inference_graph('/Users/esmepuzio/MyHouseGAN/frozen_inference_graph.pb')
+    graph = load_inference_graph('frozen_inference_graph.pb')
     image_input = graph.get_tensor_by_name('prefix/ImageTensor:0')
     print("image input shape: ", image_input.shape)
     softmax = graph.get_tensor_by_name('prefix/SemanticPredictions:0')
     print("softmax input shape: ", softmax.shape)
     if not os.path.exists(OUTPUT_PATH):
         os.makedirs(OUTPUT_PATH)
+
+        
+    # Create output directories in the image folder
+    if not os.path.exists('segmented_images_colored/'):
+        os.mkdir('segmented_images_colored/') 
+        for n in range(NUM_CLASSES):
+            os.mkdir('segmented_images_colored/class_' + str(n))
     
     print("Iterating through images")
 
     ctr = 0
-    for i, tf_image in enumerate(dataset):
-        if ctr > 5:
-            break
-        with tf.compat.v1.Session(graph=graph) as sess:
-            img = tf_image.numpy()
-            plt.imshow(img)
-            plt.show()
-            print("shape: ", img.shape)
-            print("type: ", type(img))
-            print("ok we start fr")
+    image_dir = "test_img/"
+    with tf.compat.v1.Session(graph=graph) as sess:
+        for fname in sorted(os.listdir(image_dir)):
+            img = imageio.imread(os.path.join(image_dir, fname)) 
             img = np.expand_dims(img, axis=0)
-            print("shape: ", img.shape)
+            print("img type: ", type(img))
+            print("img shape: ", img.shape)
             probs = sess.run(softmax, {image_input: img})
             img = tf.squeeze(probs).eval()
-            image_masks = logits2image(img)
-            for mask in image_masks:
-                masked_image = (cv2.bitwise_and(img, mask=mask))
-                cv2.imwrite(os.path.join(f"{OUTPUT_PATH}/img_{i}"),masked_image)
-        ctr += 1
+            img_colored = logits2image(img)
+            for i, img in enumerate(img_colored):
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                cv2.imwrite(os.path.join('segmented_images_colored/class_'+i+fname),cv2.cvtColor(img, cv2.COLOR_BGR2RGB))   
+                print(fname)
+            ctr += 1
         
 if __name__ == "__main__":
+    tf.compat.v1.enable_eager_execution()
     slice_imgs()
