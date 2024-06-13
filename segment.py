16ea7fed74a0771b5c02ac37888d1029f566465a
E. Puzio <esmepuzio@ucsb.edu>
2024-06-13 01:39:45 -0700

    Update segment.py

--- segment.py
+++ segment.py
@@ -1,19 +1,10 @@
-#https://keras.io/examples/vision/deeplabv3_plus/
+#Pretrained model and code to use pretrained model from:
 #https://github.com/hiwad-aziz/kitti_deeplab/blob/master/inference.py
-#https://github.com/rishizek/tensorflow-deeplab-v3-plus?tab=readme-ov-file
-#https://github.com/hellochick/ICNet-tensorflow
-#https://www.kaggle.com/code/kerneler/image-segmentation-cityscapes-dataset
 
 import tensorflow as tf
-from zipfile import ZipFile
-from PIL import Image
-from torchvision import transforms
-import matplotlib.pyplot as plt
-from torchdata.datapipes.iter import FileLister, FileOpener
 import matplotlib.pyplot as plt
 import os
 import sys
-import tempfile
 import imageio
 import numpy as np
 import cv2
@@ -26,6 +17,8 @@ TF_RECORD_PATH = "output.tfrecord"
 NUM_IMGS = 8
 OUTPUT_PATH = "output_images"
 NUM_CLASSES = 19
+IMAGE_DIRECTORY = "segmented_images_colored"
+OUTPUT_DIRECTORY = "collages/"
 
 
 def load_inference_graph(path):
@@ -39,7 +32,6 @@ def load_inference_graph(path):
     return graph
 
 def logits2image(logits):
-    print('logits called')
     image_classes = 19
     logits = logits.astype(np.uint8)
     images = [np.zeros([IMAGE_HEIGHT_CROPPED,IMAGE_WIDTH,3],dtype=float) for _ in range(image_classes)]
@@ -52,7 +44,7 @@ def logits2image(logits):
     images = [img.astype(np.uint8) for img in images]
     return images
 
-def slice_imgs():
+def slice_imgs(n_samples):
     print("slicing images")
     print("loading inference graph")
     graph = load_inference_graph('frozen_inference_graph.pb')
@@ -70,9 +62,8 @@ def slice_imgs():
     print("Iterating through images")
 
     image_dir = "images/"
-    # image_dir = "test_img/"
     with tf.compat.v1.Session(graph=graph) as sess:
-        for i in range(5):
+        for i in range(n_samples):
             fname=random.choice(os.listdir(image_dir))
             original_img = imageio.imread(os.path.join(image_dir, fname))[IMAGE_HEIGHT - IMAGE_HEIGHT_CROPPED:, :] 
             img = np.expand_dims(original_img, axis=0)
@@ -98,6 +89,62 @@ def slice_imgs():
                 cv2.imwrite(os.path.join('segmented_images_colored/class_'+str(i), fname[:-4]+".png"), masked_img, [int(cv2.IMWRITE_PNG_COMPRESSION), 1])
                 print(fname)
         
+
+
+# Generating collages
+def parabola(x):
+    return int((-0.0002*(x**2)) + (0.55*x) + 300)
+
+def place_imgs(img_class=int, num_imgs=int, output_name=str):
+    if not os.path.exists(OUTPUT_DIRECTORY):
+        os.mkdir(OUTPUT_DIRECTORY)
+    base_image = np.zeros((1280, 1600, 3), dtype=np.uint8)
+    base_image = np.concatenate((base_image, np.ones((1280, 1600, 1), dtype=np.uint8)), axis=2)
+    base_dims = base_image.shape[:2]
+    for _ in range(num_imgs):
+        fname = random.choice(os.listdir(IMAGE_DIRECTORY+'/class_'+str(img_class)))
+        img = imageio.imread(os.path.join(IMAGE_DIRECTORY+'/class_'+str(img_class)+"/"+ fname)) 
+        img = np.pad(img, ((0, base_dims[0] - img.shape[2]), (0,  base_dims[1] - img.shape[1]), (0, 0)), mode='constant', constant_values=0)
+
+        # #random scatter
+        # x = random.uniform(0, base_dims[1])
+        # y = random.uniform(0, base_dims[0])
+
+        #follow parabola
+        x = random.uniform(0, base_dims[1])
+        y = parabola(x) + int(random.uniform(-1.0, 1.0)*(base_dims[0]/8))
+
+        # #center
+        # x_gutter = base_dims[1]/8
+        # y_gutter = base_dims[0]/8
+        # x = random.uniform(x_gutter, base_dims[1] - x_gutter)
+        # y = random.uniform(y_gutter, base_dims[0]- y_gutter)
+
+        M = np.float32([
+            [1, 0, x],
+            [0, 1, y]
+        ])
+
+        img = cv2.warpAffine(img, M, (base_dims[1], base_dims[0]))
+        crop_type = np.random.choice([0, 1])
+        if crop_type == 0:
+            img = cv2.bitwise_not(img)
+            base_image = cv2.bitwise_and(img, base_image)
+        else:
+            base_image = cv2.bitwise_or(img, base_image)
+    cv2.imwrite(output_name, base_image)
+        
+
 if __name__ == "__main__":
-    tf.compat.v1.enable_eager_execution()
-    slice_imgs()
+    if sys.argv[1] == "slice":
+        tf.compat.v1.enable_eager_execution()
+        slice_imgs(int(sys.argv[2]))
+    if sys.argv[1] == "collage":
+        classes = int(sys.argv[3:])
+        repetitions = int(sys.argv[2])
+        for i in range(len(classes)): 
+            for j in range(repetitions):
+                place_imgs(classes[i], 10, OUTPUT_DIRECTORY+'collage_'+str((repetitions*i) + j)+'.png')
+    else:
+        print("To slice images: python segment.py slice number_of_images")
+        print("To slice images: python segment.py collage number_of_repetitions number_of_classes")
